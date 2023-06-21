22c508ba10f554ff93daf0943d8826082cc7e80c
meme escape <memescap3@gmail.com>
2023-06-21 08:37:44 +0000

    updated

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,19 +4,17 @@ const SECRET_KEY = "SOMETHING";
 const auth = (req, res, next)=>{
 
     try {
-
-
         console.log("---auth--- ",  req.headers.authorization )
     if(!req.headers.authorization || req.headers.authorization === "null"  ) {
         return res.status(200)
-        .json({ 
-            redirect: true
-        })
+        // .json({ 
+        //     redirect: true
+        // })
+        next()
     }
 
-        next();
-        
-    } catch (error) {
+       
+        } catch (error) {
         console.log(error);
         res.status(401).json({message: "Unauthorized User"});
     }
